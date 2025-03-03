0db936bdcae4f7dd4e7d8f03c9e8f67539e94ce4
Ritika Anoop <ritika1anoop@gmail.com>
2025-03-03 23:22:19 +0530

    small change

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@ export const dialogueData = {
   "sofa-table": `That's my sofa. I like to relax here and watch true crime documentaries. 
   I also enjoy learning about new technologies and staying updated with the latest trends in the tech world. When I'm not coding, you can find me diving into the cybersecurity articles or exploring new software tools.`,
   tv: `That's my TV. I love watching TV shows, movies, and documentaries. I just finished watching Unsolved Mysteries, and it was really interesting. I also just watched Deadpool and Wolverine, which was probably the best movie I've seen in years.`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  bed: `This where I sleep. Insane ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="/resume.pdf" target="_blank">Check it out?</a>
   Contact me at ritika1anoop@gmail.com if you have any interesting job opportunities!`,
   projects: `I've had the opportunity to work on some exciting projects. I am currently working on a VTOP Extension that enhances our university portal. It changes the UI/UX to make it more user friendly and accessible along with features that help students navigate the portal easily. Apart from that I have worked om DEVSOC'25 Landing which has ensured easy communication of event details with 1000+ participants.

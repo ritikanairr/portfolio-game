58e3c0d7662ba344183fe72043b6cc814d8495fa
Ritika Anoop <ritika1anoop@gmail.com>
2025-03-03 23:27:38 +0530

    change

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@ export const dialogueData = {
   "cs-degree": `This is a poster of my favourite athlete Jasprit Bumrah. Watching him play has always inspired me to keep going despite hardships.`,
   "sofa-table": `That's my sofa. I like to relax here and watch true crime documentaries. 
   I also enjoy learning about new technologies and staying updated with the latest trends in the tech world. When I'm not coding, you can find me diving into the cybersecurity articles or exploring new software tools.`,
-  tv: `That's my TV. I love watching TV shows, movies, and documentaries. I just finished watching Unsolved Mysteries, and it was really interesting. I also just watched Deadpool and Wolverine, which was probably the best movie I've seen in years.`,
+  tv: `That's my TV. I love watching TV shows, movies, and documentaries. I just finished watching Unsolved Mysteries, and it was really interesting. I also just watched Anora, which was probably the best movie I've seen in years.`,
   bed: `This where I sleep. Insane ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="/resume.pdf" target="_blank">Check it out?</a>
   Contact me at ritika1anoop@gmail.com if you have any interesting job opportunities!`,

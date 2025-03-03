bac19aa1ec0c499f6d5e02c5ade92de89c492a90
Ritika Anoop <ritika1anoop@gmail.com>
2025-03-03 23:18:17 +0530

    final hopefully

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I mainly work with Next Js. I am currently interested in Cybersecurity and I'm learning Network & Website Hacking. I've always wanted to do this :)
+    Here is my <a href="https://github.com/ritikanairr" target="_blank">Github</a>!`,
+  "cs-degree": `This is a poster of my favourite athlete Jasprit Bumrah. Watching him play has always inspired me to keep going despite hardships.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch true crime documentaries. 
+  I also enjoy learning about new technologies and staying updated with the latest trends in the tech world. When I'm not coding, you can find me diving into the cybersecurity articles or exploring new software tools.`,
+  tv: `That's my TV. I love watching TV shows, movies, and documentaries. I just finished watching Unsolved Mysteries, and it was really interesting. I also just watched Deadpool and Wolverine, which was probably the best movie I've seen in years.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="/resume.pdf" target="_blank">Check it out?</a>
+  Contact me at ritika1anoop@gmail.com if you have any interesting job opportunities!`,
+  projects: `I've had the opportunity to work on some exciting projects. I am currently working on a VTOP Extension that enhances our university portal. It changes the UI/UX to make it more user friendly and accessible along with features that help students navigate the portal easily. Apart from that I have worked om DEVSOC'25 Landing which has ensured easy communication of event details with 1000+ participants.
+  I'm always looking for new projects to work on, so feel free to reach out to me!`,
+  library: `I mostly read fiction or true crime. My shelves are filled with volumes of various series. I'm currently reading , which has been incredibly engaging. Who else loves getting lost in alternate worlds and epic adventures?`,
+  exit: `If you want to exit Ritika's portfolio, just close the tab :( `,
+};

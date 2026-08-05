4faa1242a14363e6793be64eafefee32dcbd70e4
Ritika Anoop <163532614+ritikanairr@users.noreply.github.com>
2026-08-05 18:02:16 +0530

    Update athlete references in dialogueData

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ export const dialogueData = {
   pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
     I mainly work with Next Js. I am currently interested in Cybersecurity and I'm learning Network & Website Hacking. I love doing what I do :)
     Here is my <a href="https://github.com/ritikanairr" target="_blank">Github</a>!`,
-  "cs-degree": `This is a poster of my favourite athlete Shreyas Iyer. Watching him play has always inspired me to keep going despite hardships.`,
+  "cs-degree": `This is a poster of my favourite athlete Michael Olise. Watching him play has always inspired me to keep going despite hardships.`,
   "sofa-table": `That's my sofa. I like to relax here and watch true crime documentaries. 
   I also enjoy learning about new technologies and staying updated with the latest trends in the tech world. When I'm not coding, you can find me diving into the cybersecurity articles or exploring new software tools.`,
   tv: `That's my TV. I love watching TV shows, movies, and documentaries. I just finished watching Unsolved Mysteries, and it was really interesting. I also just watched Parasite, which was probably the best movie I've seen in years.`,
@@ -13,6 +13,6 @@ export const dialogueData = {
   Contact me at ritika1anoop@gmail.com if you have any interesting job opportunities!`,
   projects: `I've had the opportunity to work on some exciting projects. I am currently working on a VTOP Extension that enhances our university portal. It changes the UI/UX to make it more user friendly and accessible along with features that help students navigate the portal easily. Apart from that I have worked om DEVSOC'25 Landing which has ensured easy communication of event details with 1000+ participants.
   I'm always looking for new projects to work on, so feel free to reach out to me!`,
-  library: `I mostly read fiction or true crime. My shelves are filled with volumes of various series. I'm currently reading , which has been incredibly engaging. Who else loves getting lost in alternate worlds and epic adventures?`,
+  library: `I mostly read fiction or true crime. My shelves are filled with volumes of various series. I'm currently reading Hunger Games, which has been incredibly engaging. Who else loves getting lost in alternate worlds and epic adventures?`,
   exit: `If you want to exit Ritika's portfolio, just close the tab `,
 };
